8f3c3d5e90447c0d10fbcf227874b92142aae386
student <sir.berento@gmail.com>
2020-02-13 11:33:12 -0600

    fixed problem with auth

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// // import LogOutButton from '../LogOutButton/LogOutButton';
+
+// // this could also be written with destructuring parameters as:
+// // const UserPage = ({ user }) => (
+// // and then instead of `props.user.username` you could use `user.username`
+
+// const UserPage = (props) => (
+//   <div >
+//     <h1 id="welcome">
+//       Welcome back, 
+//       <br />
+//       { props.user.username }!
+//     </h1>
+//     {/* <LogOutButton className="log-in" /> */}
+//   </div>
+// );
+
+// // Instead of taking everything from state, we just want the user info.
+// // if you wanted you could write this code like this:
+// // const mapStateToProps = ({user}) => ({ user });
+// const mapStateToProps = state => ({
+//   user: state.user,
+// });
+
+// // this allows us to use <App /> in index.js
+// export default connect(mapStateToProps)(UserPage);
+
+
+class HomePage extends Component {
+    
+    render() {
+        return (
+            
+                <div >
+                    <h1 id="welcome">
+                        Welcome back,
+                        <br />
+                        {this.props.user.username}!
+                     </h1>
+                    {/* <LogOutButton className="log-in" /> */}
+                </div>
+            )
+    }
+}
+
+export default HomePage
